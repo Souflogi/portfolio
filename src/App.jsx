cd1a48ec25a5f3d7817b60b40866cc49d1203f83
Soufiane <sammahsoufiane01@gmail.com>
2025-11-15 22:09:54 +0100

    Refactor deployment setup and update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
